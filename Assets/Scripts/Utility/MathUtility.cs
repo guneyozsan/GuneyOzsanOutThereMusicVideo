057a62298562a7fbdf55f1dcfcb45aea6b980230
Guney Ozsan <guneyozsan@gmail.com>
2017-09-07 20:31:21 +0300

    Introduce MathUtility class and a method for finding the closest int cube root to be used in initial generation of required number of planetesimals in cube form.

---
+++ Assets/Scripts/Utility/MathUtility.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtility {
+
+	public static int ClosestCubeRoot(int source, bool inclusive)
+    {
+        int root = 0;
+        float rootPower = Mathf.Pow(root, 3);
+
+        while (rootPower < source)
+        {
+            root++;
+            rootPower = Mathf.Pow(root, 3);
+        }
+
+        if (inclusive)
+        {
+            return root;
+        }
+        else
+        {
+            return root - 1;
+        }
+    }
+
+}
