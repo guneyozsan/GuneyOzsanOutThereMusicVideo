1761ad8f1ae36046711a13e69ce726ff9733bd79
Guney Ozsan <guneyozsan@gmail.com>
2017-11-02 23:50:10 +0200

    Rename DebugGUI class to Debugging.

--- Assets/Scripts/Debugging.cs
+++ Assets/Scripts/Debugging.cs
@@ -22,7 +22,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugGUI : MonoBehaviour {
+public class Debugging : MonoBehaviour {
 
     [SerializeField]
     bool displayGui;
