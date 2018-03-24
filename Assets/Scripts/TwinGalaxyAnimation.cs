db714585c2e0531438fd4f5970fa0a1692866e66
Guney Ozsan <guneyozsan@gmail.com>
2018-03-24 17:05:23 +0300

    Tweak twin galaxy animation, bars 32-56.

--- Assets/Scripts/TwinGalaxyAnimation.cs
+++ Assets/Scripts/TwinGalaxyAnimation.cs
@@ -16,17 +16,18 @@ public class TwinGalaxyAnimation {
         float sequenceLength = Sequencer.BarDuration * (lastBar + 1 - firstBar);
         float rotationSpeed = 0.003f;
 
-        float initialR = 18f;
-        float maxR = 55f;
-        float rGrowthPower = 0.5f;
+        float initialR = 55f;
+        float maxR = 0f;
+        float rGrowthPower = 7f;
         float r = initialR + (maxR - initialR) * (Mathf.Pow(t, rGrowthPower) / Mathf.Pow(sequenceLength, rGrowthPower));
+        Debug.Log(r);
 
         float radialOffset = 0.6f;
         Vector3 orbit = new Vector3(
                     r * Mathf.Cos(radian + radialOffset),
             0.37f * r * Mathf.Cos(radian + radialOffset),
                    -r * Mathf.Sin(radian + radialOffset));
-        Vector3 offset = new Vector3(0, 0, +20f);
+        Vector3 offset = new Vector3(0, 0, 40f);
         targets = new List<Vector3>() {
              orbit + offset,
             -orbit + offset
@@ -37,7 +38,7 @@ public class TwinGalaxyAnimation {
         float forceGrowthPower = 1f / 2.8f;
         float force = maxGravityForce * Mathf.Pow(t, forceGrowthPower) / Mathf.Pow(sequenceLength, forceGrowthPower);
 
-        AnimationManager.SetGravityPerBar(new float[] { 1.55f * force, 2.0f * force}, targets, 1, firstBar );
+        AnimationManager.SetGravityPerBar(new float[] { 2.0f * force}, targets, 1, firstBar );
         t += Time.deltaTime;
     }
 }
