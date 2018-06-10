48c57697e331a619d14aeff9cb810da84288d4ab
Guney Ozsan <guneyozsan@gmail.com>
2018-06-10 21:53:08 +0300

    Customize recorder script.

--- Assets/Scripts/Utility/FrameRecorder.cs
+++ Assets/Scripts/Utility/FrameRecorder.cs
@@ -1,31 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Based on https://docs.unity3d.com/ScriptReference/Time-captureFramerate.html
+
 // Capture frames as a screenshot sequence. Images are
 // stored as PNG files in a folder - these can be combined into
 // a movie using image utility software (eg, QuickTime Pro).
 
 public class FrameRecorder : MonoBehaviour
 {
-    // The folder to contain our screenshots.
-    // If the folder exists we will append numbers to create an empty folder.
-    public string folder = "ScreenshotFolder";
-    public int frameRate = 25;
+    [SerializeField]
+    bool enabled;
+
+    [SerializeField]
+    string folder = "CapturedRecordings";
+
+    [SerializeField]
+    int frameRate = 30;
+
     void Start()
     {
-        // Set the playback framerate (real time will not relate to game time after this).
-        Time.captureFramerate = frameRate;
-
-        // Create the folder
-        System.IO.Directory.CreateDirectory(folder);
+        if (enabled)
+        {
+            // Set the playback framerate (real time will not relate to game time after this).
+            Time.captureFramerate = frameRate;
+            System.IO.Directory.CreateDirectory(folder);
+        }
     }
 
     void Update()
     {
-        // Append filename to folder name (format is '0005 shot.png"')
-        string name = string.Format("{0}/{1:D04} shot.png", folder, Time.frameCount);
-
-        // Capture the screenshot to the specified file.
-        ScreenCapture.CaptureScreenshot(name);
+        if (enabled)
+        {
+            string name = string.Format("{0}/Fame-{1:D04}.png", folder, Time.frameCount);
+            ScreenCapture.CaptureScreenshot(name);
+        }
     }
 }
