1e67d2cc7e6cdf7df1dd890880b2b59e4bdf4a6a
Guney Ozsan <guneyozsan@gmail.com>
2018-07-17 03:53:57 +0300

    First draft for independent sequencer.

---
+++ Assets/Scripts/Utility/Sequencer/MusicTime.cs
@@ -0,0 +1,60 @@
+﻿// Audio Sequencer - An audio sequencer tool for Unity 3D
+// Copyright (C) 2018 Guney Ozsan
+
+// This file is part of Audio Sequencer.
+
+// Audio Sequencer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ---------------------------------------------------------------------
+
+namespace PostIllusions.Audio
+{
+    public struct MusicTime
+    {
+        public  int     Bar     { get; set; }
+        public  int     Beat    { get; set; }
+        public  float   Miliseconds { get; set; }
+
+        public void AddBeat(int value)
+        {
+            Beat += value;
+        }
+
+        public void AddBar(int value)
+        {
+            Bar += value;
+        }
+
+        public void AddTime(float value)
+        {
+            Miliseconds += value;
+        }
+
+        public void SetBeat(int value)
+        {
+            Beat = value;
+        }
+
+        public void SetMiliseconds(float value)
+        {
+            Miliseconds = value;
+        }
+
+        public void Reset()
+        {
+            Bar     = 1;
+            Beat    = 1;
+            Miliseconds = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Sequencer/Sequencer.cs
@@ -0,0 +1,86 @@
+﻿// Audio Sequencer - An audio sequencer tool for Unity 3D
+// Copyright (C) 2018 Guney Ozsan
+
+// This file is part of Audio Sequencer.
+
+// Audio Sequencer is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ---------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PostIllusions.Audio
+{
+    public partial class Sequencer : MonoBehaviour
+    {
+        public static   MusicTime   MusicTime { get; private set; }
+        
+        [SerializeField]
+        private         float       bpm;
+        public          float       Bpm { get { return bpm; } }
+
+        private         bool        isPlaying;
+
+        private void Update()
+        {
+            if (isPlaying)
+                MusicTime.AddTime(Time.deltaTime);
+        }
+
+        public void Play()
+        {
+            isPlaying = true;
+        }
+
+        public void Pause()
+        {
+            isPlaying = false;
+        }
+
+        public void Stop()
+        {
+            isPlaying = false;
+            MusicTime.Reset();
+        }
+
+        void SetBeats()
+        {
+            double timeOfCurrentBeat = ((MusicTime.Bar - 1) * 4 + MusicTime.Beat) * BeatDuration;
+
+#if UNITY_EDITOR
+            timeOfCurrentBeat -= BeatDuration * (1 - 1 / Time.timeScale);
+#endif // UNITY_EDITOR
+
+            if (MusicTime.Miliseconds > timeOfCurrentBeat)
+            {
+                if (MusicTime.Beat < 4)
+                {
+                    MusicTime.AddBeat(1);
+                }
+                else
+                {
+                    MusicTime.SetBeat(1);
+                    MusicTime.AddBar(1);
+                }
+
+#if UNITY_EDITOR
+                if (UnityEngine.Time.timeScale != 1)
+                    MusicTime.SetMiliseconds((float)(timeOfCurrentBeat + BeatDuration * (1 - 1 / Time.timeScale)));
+#endif // UNITY_EDITOR
+
+            }
+        }
+    }
+}
