59e03938f86b0fb31a94e867dff27c0f063ab4e1
Guney Ozsan <guneyozsan@gmail.com>
2021-01-10 21:32:59 +0300

    Refactor rename Debugging to SequencerEditorController and move to Sequencer game object

--- Assets/Scripts/SequencerEditorController.cs
+++ Assets/Scripts/SequencerEditorController.cs
@@ -16,11 +16,9 @@
 // ---------------------------------------------------------------------
 
 # if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Debugging : MonoBehaviour {
+public class SequencerEditorController : MonoBehaviour {
 
     [SerializeField]
     [Range(0, 5)]
